60df6bf8716b23f313d00407dd3255255245124d
ibrahimasy11 <ibhmsy11@gmail.com>
2025-10-17 10:31:56 -0400

    “updated”

--- index.js
+++ index.js
@@ -13,6 +13,7 @@
 
 const containsDuplicate = (nums) => {
   //write your code here
+
 };
 
 // Export the function for testing
@@ -22,3 +23,39 @@ module.exports = { containsDuplicate };
 // console.log(containsDuplicate([1,2,3,1])); // → true
 // console.log(containsDuplicate([1,2,3,4])); // → false
 // console.log(containsDuplicate([1,1,1,3,3,4,3,2,4,2])); // → true
+
+/* note 
+PEDAC
+
+Problem:
+We need to determine if any number appears at least twice in the array
+If yes number did repeat return true
+If no number did not reapeat eturn false
+
+Example:
+console.log(containsDuplicate([1,2,3,1])); // → true because number 1 reapeated 
+console.log(containsDuplicate([1,2,3,4])); // → false because no number reapeated 
+console.log(containsDuplicate([1,1,1,3,3,4,3,2,4,2])); // → true because multiple number repeated
+
+Data structure 
+use object store counts of numbers we have seen
+
+Algorithm
+Create an empty object counts = {}
+Loop through each number in the array
+For each number: If it already exists in the object → return true 
+Otherwise false
+
+Code:
+
+const containsDuplicate = (nums) => {
+  //write your code here
+  const seen = {}
+
+  for (let i = 0; i < nums.lenght; i++){
+    const num
+  }
+
+};
+
+*/
